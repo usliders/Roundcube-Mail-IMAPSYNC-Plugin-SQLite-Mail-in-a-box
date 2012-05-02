825d1435cbcb14a90a3b43845475e231c89a51e7
Thorsten Puzich <thorsten@puzich.com>
2012-05-02 18:02:43 +0200

    little change on fetchmail.pl

--- fetchmail/scripts/fetchmail.pl
+++ fetchmail/scripts/fetchmail.pl
@@ -14,8 +14,8 @@ use LockFile::Simple qw(lock trylock unlock);
 # database settings
 
 # database backend - uncomment one of these
-our $db_type = 'Pg';
-#my $db_type = 'mysql';
+#our $db_type = 'Pg';
+my $db_type = 'mysql';
 
 # host name
 our $db_host="127.0.0.1";
@@ -29,8 +29,8 @@ our $db_password="CHANGE_ME!";
 # instead of changing this script, you can put your settings to /etc/mail/postfixadmin/fetchmail.conf
 # just use perl syntax there to fill the variables listed above (without the "our" keyword). Example:
 # $db_username = 'mail';
-if (-f "/etc/mail/postfixadmin/fetchmail.conf") {
-	require "/etc/mail/postfixadmin/fetchmail.conf";
+if (-f "/etc/mail/fetchmail.conf") {
+	require "/etc/mail/fetchmail.conf";
 }
 
 
