efd80fcbb11821d725a605798adee85f8c86d769
pf4public <PF4Public@users.noreply.github.com>
2016-02-17 23:56:21 +0300

    MySQL and PostgreSQL support

---
+++ fetchmail/scripts/fetchmail.pgsql.sql
@@ -0,0 +1,26 @@
+CREATE TABLE fetchmail (
+    id integer NOT NULL,
+    mailbox text NOT NULL,
+    active integer DEFAULT 1 NOT NULL,
+    src_server text NOT NULL,
+    src_auth text DEFAULT 'password'::text NOT NULL,
+    src_user text NOT NULL,
+    src_password text NOT NULL,
+    src_folder text,
+    poll_time integer DEFAULT 10 NOT NULL,
+    fetchall integer DEFAULT 0 NOT NULL,
+    keep integer DEFAULT 1 NOT NULL,
+    protocol text DEFAULT 'IMAP'::text NOT NULL,
+    usessl integer DEFAULT 0 NOT NULL,
+    extra_options text,
+    returned_text text,
+    mda text,
+    date timestamp without time zone DEFAULT now() NOT NULL
+);
+
+CREATE SEQUENCE fetchmail_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
