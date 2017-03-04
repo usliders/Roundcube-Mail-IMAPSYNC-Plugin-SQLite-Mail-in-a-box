c57db04ea2055c69696876ddd2ac857c0715cf3f
DaCHRIS <dachrls0@gmail.com>
2017-03-04 01:43:56 +0100

    Update postgres.initial.sql

--- SQL/postgres.initial.sql
+++ SQL/postgres.initial.sql
@@ -1,4 +1,4 @@
-CREATE TABLE fetchmail (
+CREATE TABLE imapsync (
     id integer NOT NULL,
     mailbox character varying(255) DEFAULT ''::character varying NOT NULL,
     active BOOLEAN DEFAULT 't' NOT NULL,
@@ -7,10 +7,14 @@ CREATE TABLE fetchmail (
     src_user character varying(255) DEFAULT ''::character varying NOT NULL,
     src_password character varying(255) DEFAULT ''::character varying NOT NULL,
     src_folder character varying(255) DEFAULT ''::character varying NOT NULL,
+    dest_password character varying(255) DEFAULT ''::character varying NOT NULL,
     poll_time integer DEFAULT 10 NOT NULL,
     fetchall BOOLEAN DEFAULT 'f' NOT NULL,
     keep BOOLEAN DEFAULT 't' NOT NULL,
     protocol character varying(15) DEFAULT 'IMAP'::character varying NOT NULL,
+    subscribeall BOOLEAN DEFAULT 't' NOT NULL,
+    skipempty BOOLEAN DEFAULT 't' NOT NULL,
+    maxage integer DEFAULT 365 NOT NULL,
     usessl BOOLEAN DEFAULT 't' NOT NULL,
     sslcertck BOOLEAN DEFAULT 'f' NOT NULL,
     sslcertpath character varying(255) DEFAULT ''::character varying,
@@ -19,19 +23,19 @@ CREATE TABLE fetchmail (
     returned_text text,
     mda character varying(255) DEFAULT ''::character varying NOT NULL,
     date timestamp with time zone DEFAULT now() NOT NULL
-    CONSTRAINT fetchmail_protocol_check CHECK (((protocol)::text = ANY (ARRAY[('POP3'::character varying)::text, ('IMAP'::character varying)::text, ('POP2'::character varying)::text, ('ETRN'::character varying)::text, ('AUTO'::character varying)::text]))),
-    CONSTRAINT fetchmail_src_auth_check CHECK (((src_auth)::text = ANY (ARRAY[('password'::character varying)::text, ('kerberos_v5'::character varying)::text, ('kerberos'::character varying)::text, ('kerberos_v4'::character varying)::text, ('gssapi'::character varying)::text, ('cram-md5'::character varying)::text, ('otp'::character varying)::text, ('ntlm'::character varying)::text, ('msn'::character varying)::text, ('ssh'::character varying)::text, ('any'::character varying)::text])))
+    CONSTRAINT imapsync_protocol_check CHECK (((protocol)::text = ANY (ARRAY[('POP3'::character varying)::text, ('IMAP'::character varying)::text, ('POP2'::character varying)::text, ('ETRN'::character varying)::text, ('AUTO'::character varying)::text]))),
+    CONSTRAINT imapsync_src_auth_check CHECK (((src_auth)::text = ANY (ARRAY[('password'::character varying)::text, ('kerberos_v5'::character varying)::text, ('kerberos'::character varying)::text, ('kerberos_v4'::character varying)::text, ('gssapi'::character varying)::text, ('cram-md5'::character varying)::text, ('otp'::character varying)::text, ('ntlm'::character varying)::text, ('msn'::character varying)::text, ('ssh'::character varying)::text, ('any'::character varying)::text])))
 );
 
-CREATE SEQUENCE fetchmail_id_seq
+CREATE SEQUENCE imapsync_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
 	
-ALTER SEQUENCE fetchmail_id_seq OWNED BY fetchmail.id;
+ALTER SEQUENCE imapsync_id_seq OWNED BY imapsync.id;
 
-ALTER TABLE ONLY fetchmail ALTER COLUMN id SET DEFAULT nextval('fetchmail_id_seq'::regclass);
+ALTER TABLE ONLY imapsync ALTER COLUMN id SET DEFAULT nextval('imapsync_id_seq'::regclass);
 
-ALTER TABLE ONLY fetchmail ADD CONSTRAINT fetchmail_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY imapsync ADD CONSTRAINT imapsync_pkey PRIMARY KEY (id);
